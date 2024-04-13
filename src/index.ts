cf352ba009a8e37c44d95e27a8584a28cbb6ad51
1pp0 <34667432+k1pp0@users.noreply.github.com>
2024-04-13 13:46:33 +0000

    feat: handle slack app mention events and reply with "hello"

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  SLACK_BOT_USER_OAUTH_TOKEN: string
+}
+const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', async(c) => {
+  const req = await c.req.json()
+
+  if (req.type === 'url_verification') {
+    return c.text(req.challenge)
+  }
+
+  if (req.event.type === 'app_mention') {
+    const token = c.env.SLACK_BOT_USER_OAUTH_TOKEN
+    const channelId = req.event.channel
+    const threadTs = req.event.thread_ts || req.event.ts
+    const url = 'https://slack.com/api/chat.postMessage'
+    const params = {
+      channel: channelId,
+      thread_ts: threadTs,
+      text: 'hello',
+    }
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(params),
+    })
+    return c.text("ok")
+  }
 })
 
 export default app
