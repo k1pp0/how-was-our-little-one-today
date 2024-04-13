8e8c75e6a5682f2c078ecec175d1375100d8c516
1pp0 <34667432+k1pp0@users.noreply.github.com>
2024-04-13 14:51:40 +0000

    feat: get all messages from the thread in which the bot is mentioned

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,28 @@ type Bindings = {
 }
 const app = new Hono<{ Bindings: Bindings }>()
 
+interface SlackMessage {
+  type: string;
+  user: string;
+  text: string;
+  thread_ts: string;
+  reply_count?: number;
+  subscribed?: boolean;
+  last_read?: string;
+  unread_count?: number;
+  ts: string;
+  parent_user_id?: string;
+}
+
+interface SlackConversationsRepliesResponse {
+  messages: SlackMessage[];
+  has_more: boolean;
+  ok: boolean;
+  response_metadata: {
+    next_cursor: string;
+  }
+}
+
 app.post('/', async(c) => {
   const req = await c.req.json()
 
@@ -13,25 +35,49 @@ app.post('/', async(c) => {
   }
 
   if (req.event.type === 'app_mention') {
-    const token = c.env.SLACK_BOT_USER_OAUTH_TOKEN
-    const channelId = req.event.channel
-    const threadTs = req.event.thread_ts || req.event.ts
-    const url = 'https://slack.com/api/chat.postMessage'
-    const params = {
-      channel: channelId,
-      thread_ts: threadTs,
-      text: 'hello',
-    }
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-      body: JSON.stringify(params),
-    })
+    const token: string = c.env.SLACK_BOT_USER_OAUTH_TOKEN
+    const channelId: string = req.event.channel
+    const threadTs: string = req.event.thread_ts || req.event.ts
+
+    const messages = await slackConversationsReplies(
+      token, channelId, threadTs
+    )
+
+    await slackChatPostMessage(
+      token, channelId, threadTs, `hello: ${messages.length}`
+    )
+
     return c.text("ok")
   }
 })
 
 export default app
+
+async function slackChatPostMessage(token: string, channelId: string, threadTs: string, text: string){
+  const url = 'https://slack.com/api/chat.postMessage'
+  const params = {
+    channel: channelId,
+    thread_ts: threadTs,
+    text: text,
+  }
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify(params),
+  })
+}
+
+async function slackConversationsReplies(token: string, channelId: string, threadTs: string): Promise<SlackMessage[]> {
+  const url = `https://slack.com/api/conversations.replies?channel=${channelId}&ts=${threadTs}`
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  })
+  const data: SlackConversationsRepliesResponse = await response.json()
+  return data.messages
+}
